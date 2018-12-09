dcf061ef01c166c3013c3936d085ca482029c5c9
arbaaz-khan <32384702+arbaaz-khan@users.noreply.github.com>
2018-12-09 16:23:31 +0530

    Updated login.js
    Changed the redirect url for the game page.

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,7 @@ function btnClicked(){
     if(inputField[0].value.trim() !=""){
         inputField[0].classList.remove('error');
         userName=inputField[0].value.trim();
-        window.location.href ="arbaaz-khan-github.io/gameOn.htm"+"?user="+userName;
+        window.location.href ="arbaaz-khan.github.io/gameOn.html"+"?user="+userName;
     }else{
         inputField[0].classList.add('error');
     }
